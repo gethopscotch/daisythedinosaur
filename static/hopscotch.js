f50534640ca5657293d67f1a4c73e1942541299d
Evan Farrar & Sam John <pair+evan+samantha@hopscotchapp.com>
2011-10-16 19:44:20 -0400

    drag and drop for iPad

--- static/hopscotch.js
+++ static/hopscotch.js
@@ -1,11 +1,11 @@
 var Hopscotch = {
   init: function() {
-    this.paper = Raphael($("#print-area"), 50, 600, 600);
+    this.paper = Raphael("print-area");
     this.circle = this.paper.circle(50, 40, 10);
     this.circle.attr("fill", "#f00");
   },
 
-  moveLeft: function() {
+  moveRight: function() {
     var anim = this.circle.animate({cx: this.circle.attr('cx') + 100}, 500, 'linear');
   },
 
@@ -13,10 +13,67 @@ var Hopscotch = {
     return _.map($("#command-list .command"), function(command) {
       return $(command).html();
     });
+  },
+
+  downCallback: function() {
+    var $self = $(this).clone();
+    $self.css("position", "absolute");
+    $self.offset($(this).offset());
+    $("body").append($self);
+    var move = function(e) {
+      $self.css("top", e.pageY);
+      $self.css('left', e.pageX);
+    }
+    $("body").bind("touchmove", function(e) {
+      e.preventDefault();
+      var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+      move(touch);
+    });
+    $("body").bind("mousemove", function(e) {
+      e.preventDefault();
+      move(e);
+    });
+
+    $('body').bind('touchend', function(e) {
+      Hopscotch.upCallback($self, e);
+    });
+
+    $('body').bind('mouseup', function(e) {
+      Hopscotch.upCallback($self, e);
+    });
+  },
+
+  upCallback: function(element, e) {
+    $cl = $("#command-list");
+    var height = $cl.height();
+    var width = $cl.width();
+    var topPos = $cl.position().top;
+    var leftPos = $cl.position().left;
+
+    if ($(element).position().left >= leftPos && $(element).position().left <= leftPos + width && $(element).position().top >= topPos && $(element).position().top <= topPos + height) {
+      $("#command-list").append(element);
+      element.css({
+        'position': 'relative',
+        'top' : '0',
+        'left': '0'
+      });
+    } else {
+      element.remove();
+    }
+    $("body").unbind("mouseup");
+    $("body").unbind("touchend");
+    $("body").unbind("mousemove");
+    $("body").unbind("touchmove");
   }
 }
 $(function() {
   Hopscotch.init();
+
+  $('#methods .command').live('mousedown', Hopscotch.downCallback);
+  $("#methods .command").live('touchstart', Hopscotch.downCallback);
+
+
+
 
   $("#play").click(function(){
     _.each(Hopscotch.commandsToRun(), function(command, index) {
