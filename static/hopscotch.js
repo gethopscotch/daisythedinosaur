524a0bb6e6413c272df02e2c5f6be10637741625
Evan Farrar & Sam John <pair+evan+samantha@hopscotchapp.com>
2011-10-16 20:24:44 -0400

    added a trash button

--- static/hopscotch.js
+++ static/hopscotch.js
@@ -34,7 +34,7 @@ var Hopscotch = {
     });
   },
 
-  downCallback: function() {
+  methodDownCallback: function() {
     var $self = $(this).clone();
     $self.css("position", "absolute");
     $self.offset($(this).offset());
@@ -54,15 +54,43 @@ var Hopscotch = {
     });
 
     $('body').bind('touchend', function(e) {
-      Hopscotch.upCallback($self, e);
+      Hopscotch.upCallbackCommandList($self, e);
     });
 
     $('body').bind('mouseup', function(e) {
-      Hopscotch.upCallback($self, e);
+      Hopscotch.upCallbackCommandList($self, e);
     });
   },
 
-  upCallback: function(element, e) {
+  listDownCallback: function() {
+    var $self = $(this);
+    var oldOffset = $(this).offset();
+    $self.css("position", "absolute");
+    $self.offset(oldOffset);
+    var move = function(e) {
+      $self.css("top", e.pageY);
+      $self.css('left', e.pageX);
+    }
+    $("body").bind("touchmove", function(e) {
+      e.preventDefault();
+      var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+      move(touch);
+    });
+    $("body").bind("mousemove", function(e) {
+      e.preventDefault();
+      move(e);
+    });
+
+    $('body').bind('touchend', function(e) {
+      Hopscotch.upCallbackTrash($self, e);
+    });
+
+    $('body').bind('mouseup', function(e) {
+      Hopscotch.upCallbackTrash($self, e);
+    });
+  },
+
+  upCallbackCommandList: function(element, e) {
     $cl = $("#command-list");
     var height = $cl.height();
     var width = $cl.width();
@@ -83,13 +111,38 @@ var Hopscotch = {
     $("body").unbind("touchend");
     $("body").unbind("mousemove");
     $("body").unbind("touchmove");
+  },
+
+  upCallbackTrash: function(element, e) {
+    $cl = $("#trash-bin");
+    var height = $cl.height();
+    var width = $cl.width();
+    var topPos = $cl.position().top;
+    var leftPos = $cl.position().left;
+
+    if ($(element).position().left >= leftPos && $(element).position().left <= leftPos + width && $(element).position().top >= topPos && $(element).position().top <= topPos + height) {
+      element.remove();
+    } else {
+      $("#command-list").append(element);
+      element.css({
+        'position': 'relative',
+        'top' : '0',
+        'left': '0'
+      });
+    }
+    $("body").unbind("mouseup");
+    $("body").unbind("touchend");
+    $("body").unbind("mousemove");
+    $("body").unbind("touchmove");
   }
 }
 $(function() {
   Hopscotch.init();
 
-  $('#methods .command').live('mousedown', Hopscotch.downCallback);
-  $("#methods .command").live('touchstart', Hopscotch.downCallback);
+  $('#command-list .command').live('mousedown', Hopscotch.listDownCallback);
+  $("#command-list .command").live('touchstart', Hopscotch.listDownCallback);
+  $('#methods .command').live('mousedown', Hopscotch.methodDownCallback);
+  $("#methods .command").live('touchstart', Hopscotch.methodDownCallback);
 
 
 
