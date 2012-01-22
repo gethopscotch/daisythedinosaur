db8f6f07b6b7e6bc031b042bbf6f9a395df89587
Sam John <pair+samantha@gethopscotch.com>
2012-01-22 18:35:47 +0000

    Add styling to tutorial and add a success message.

--- static/javascripts/tutorial.js
+++ static/javascripts/tutorial.js
@@ -16,7 +16,21 @@ var Step = {
       e.remove();
     })
   },
-  elements: []
+  elements: [],
+  success: function(callback) {
+    $('#success').dialog({
+      modal: true,
+      dialogClass: 'success',
+      width: 600,
+      title: "Congratulations!",
+      buttons: {
+        "Try the next challenge": function() {
+          $(this).dialog('close');
+          callback();
+        }
+      }
+    });
+  }
 }
 
 $(function(){
@@ -37,35 +51,49 @@ var Tutorial = {
       }
       $('#current-step').attr('class', 'step-'+currentStep);
       Tutorial.steps[currentStep].draw();
-      $('.step-dialog'+currentStep).dialog({modal: true, title: Tutorial.steps[currentStep].title});
+      $('.step-dialog'+currentStep).dialog({
+        modal: true, 
+        title: Tutorial.steps[currentStep].title,
+        buttons: {
+          "Let's go!": function() {
+            $(this).dialog('close');
+          }
+        }
+      });
     }
   },
   runStepSpec: function() {
     if (Tutorial.steps[currentStep].spec()) {
+      Tutorial.steps[currentStep].success(Tutorial.nextStep);
+    }
+  },
+  nextStep: function() {
+    Stage.dino.animate({x: Stage.position.x, y: Stage.position.y}, 0, 'linear', function() {
       Tutorial.steps[currentStep].undraw();
       window.localStorage.setItem("current-step", currentStep + 1);
       currentStep = parseInt(window.localStorage.getItem("current-step"));
       setTimeout(function() {Tutorial.promptStep()},400);
-    }
+    });
   },
   steps: [
     $.extend({}, Step, {
       title: "First Steps",
       description: "Hello and welcome to hopscotch!<br/><br/>"+
-                   "Try figuring out how to move Daisy into the red circle.",
+                   "Try figuring out how to move Daisy so that she stops in the center of the circle.",
       spec: function() {
-        return ($('.program .command').length == 1 &&
-        $('.program .move select').val() == "Forward");
+
+        return (Stage.dino.attr('x') == 150 &&
+                Stage.dino.attr('y') == 140);
       },
       draw: function() {
         this.elements = [
-          Stage.paper.circle(190,175,50).attr({'stroke-width': 4, 'stroke': 'red'})
+          Stage.paper.circle(190,175,50).attr({'stroke-width': 4, 'stroke': '#F6498A'})
         ];
       }
     }),
     $.extend({}, Step, {
       title: "Reach for the clouds!",
-      description: "Now the red circle is a little higher. Use the jump method to reach it.",
+      description: "Now the circle is a little higher. Use the jump method to reach it.",
       spec: function() {
         var gotSteps = _.map($('.program .command .name'), function(command){return $(command).html()});
         var expectedSteps = ["move", "jump"];
@@ -73,7 +101,7 @@ var Tutorial = {
       },
       draw: function() {
         this.elements = [
-          Stage.paper.circle(200,135,40).attr({'stroke-width': 4, 'stroke': 'red'})
+          Stage.paper.circle(200,135,40).attr({'stroke-width': 4, 'stroke': '#F6498A'})
         ];
       }
     }),
