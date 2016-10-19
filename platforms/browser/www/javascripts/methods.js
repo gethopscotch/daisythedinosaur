86ee1d15f718a23c8ce78ba95fd50468b1c7b847
Samantha John <sam@gethopscotch.com>
2016-10-19 10:22:22 -0400

    Setup for cordova phonegap

---
+++ platforms/browser/www/javascripts/methods.js
@@ -0,0 +1,244 @@
+var Method = {
+  args: [],
+  call: function() {}
+}
+
+var Methods = {
+  "repeat": $.extend({}, Method, {call: function(arg, callback, command, commandList) {
+  }}),
+  when: $.extend({}, Method, {args: ['shake', 'touch'], call: function(arg, callback, command, commandList) {
+    PrivateMethods[arg](callback, command, commandList);
+  }}),
+  move: $.extend({}, Method, {args: ['Forward', 'Backward'], call: function(arg, callback, command, commandList) {
+    PrivateMethods["move" + arg](function() {callback(command, commandList)});
+  }}),
+
+  turn: $.extend({}, Method, {call: function(args, callback, command, commandList) {
+    if (Stage.dinoDirection == "left") {
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/back.png')  }, 150);
+      setTimeout(function(){
+                  Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png');
+                  Stage.dinoDirection = "right";
+                  callback(command, commandList);
+      }, 300);
+    } else if(Stage.dinoDirection == "right") {
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/front.png')  }, 150)
+      setTimeout(function(){
+        Stage.dinoDirection = "left"
+        Stage.dino.attr('src', 'images/'+ Stage.spriteDir +'/l1.png');
+        callback(command, commandList);
+      }, 300)
+    };
+  }}),
+
+  grow: $.extend({}, Method, {call: function(args, callback, command, commandList) {
+    var scale = Stage.dino.data("scale");
+    if (scale == 2) {
+      Stage.spriteDir = "sprites_lg";
+      src = Stage.dino.attr('src');
+      Stage.dino.attr('src', src.split('/sprites/').join('/sprites_lg/'))
+    }
+    var height = Stage.dino.attrs.height;
+    var width = Stage.dino.attrs.width;
+    console.log(height*1.25)
+    console.log(180 - 0.5 * (height * 1.25))
+    Stage.dino.animate({width: width * 1.25, height: height * 1.25, y: (194 - 0.66 * (height * 1.25))}, 500, function(){
+      Stage.dino.data("scale", scale + 1);
+      callback(command, commandList);
+    });
+  }}),
+  shrink: $.extend({}, Method, {call: function(args, callback, command, commandList) {
+    var scale = Stage.dino.data("scale");
+    if (scale == 2) {
+      Stage.spriteDir = "sprites";
+      src = Stage.dino.attr('src');
+      Stage.dino.attr('src', src.split('/sprites_lg/').join('/sprites/'))
+    }
+    var height = Stage.dino.attrs.height
+    var width = Stage.dino.attrs.width
+    Stage.dino.animate({width: width*0.8, height: height*0.8, y: (200 - 0.66 * (height * 0.8))}, 500, function(){
+      Stage.dino.data("scale", scale - 1);
+      callback(command, commandList);
+    });
+  }}),
+
+  jump: $.extend({}, Method, {call:
+                 function(args, callback, command, commandList) {
+    if  (Stage.dinoDirection == "right") {
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/3.png')  }, 10);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/4.png')  }, 250);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png')  }, 500);
+      Stage.dino.animate(
+        {y: Stage.dino.attr('y') - 50}, 250, 'linear',
+        function(){
+          Stage.dino.animate({y: Stage.dino.attr('y') + 50}, 250, 'linear', function() {callback(command, commandList)})
+        });
+    } else if (Stage.dinoDirection == "left") {
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l3.png')  }, 10);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l4.png')  }, 250);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l1.png')  }, 500);
+      Stage.dino.animate(
+        {y: Stage.dino.attr('y') - 50}, 250, 'linear',
+        function(){ Stage.dino.animate({y: Stage.dino.attr('y') + 50}, 250, 'linear', function() {callback(command, commandList)}) });
+    }
+  }
+  }),
+
+  roll: $.extend({}, Method, {call: function(arg, callback, command, commandList) {
+    if  (Stage.dinoDirection == "right") {
+      var anim = Stage.dino.animate(
+        {transform: "r360,"+(Stage.dino.width / 2)+","+(Stage.dino.height / 2)},
+        500, 'linear', function(){
+          Stage.dino.transform("r0");
+          callback(command, commandList);
+        });
+    } else if (Stage.dinoDirection == "left") {
+      var anim = Stage.dino.animate(
+        {transform: "r-360,"+(-Stage.dino.width / 2)+","+(Stage.dino.height / 2)},
+        500, 'linear', function(){
+          Stage.dino.transform("r0");
+          callback(command, commandList);
+        });
+    }
+  }}),
+
+  spin: $.extend({}, Method, {call: function(args, callback, command, commandList) {
+    if (Stage.dinoDirection == "left") {
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/back.png')  }, 150);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png'); }, 300);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/front.png')  }, 450)
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l1.png'); }, 600);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/back.png')  }, 750);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png'); }, 900);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/front.png')  }, 1050)
+      setTimeout(function(){
+        Stage.dino.attr('src', 'images/'+ Stage.spriteDir +'/l1.png');
+        callback(command, commandList);
+      }, 1250);
+    } else if(Stage.dinoDirection == "right") {
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/front.png')  }, 150);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l1.png'); }, 300);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/back.png')  }, 450)
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png'); }, 600);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/front.png')  }, 750);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l1.png'); }, 900);
+      setTimeout(function(){ Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/back.png')  }, 1050)
+      setTimeout(function(){
+        Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png');
+        callback(command, commandList);
+      }, 1250);
+    }
+  }})
+};
+
+var PrivateMethods = {
+  moveForward: function(callback) {
+    var x = Stage.dino.attr('x');
+    if  (Stage.dinoDirection == "left") {
+      PrivateMethods.animateLeft(1, x, -10, callback);
+    } else if (Stage.dinoDirection == "right") {
+      PrivateMethods.animateRight(1, x, 10, callback);
+    }
+
+  },
+
+  moveBackward: function(callback) {
+    var x = Stage.dino.attr('x');
+    if  (Stage.dinoDirection == "left") {
+      PrivateMethods.animateLeft(1, x, 10, callback);
+    } else if (Stage.dinoDirection == "right") {
+      PrivateMethods.animateRight(1, x, -10, callback);
+    }
+  },
+
+  animateRight: function(index, x, offset, callback) {
+    if (x + offset > Stage.width - 63  || x + offset < 0) {
+      offset = 0;
+    }
+    if (index < 10) {
+      Stage.dino.animate({x: x+offset}, 100, 'linear', function() {
+        if  (Stage.dinoDirection == "left") {
+          Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l' + index + '.png');
+          PrivateMethods.animateLeft(index + 1, x - offset, -offset, callback);
+        } else {
+          Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/' + index + '.png');
+          PrivateMethods.animateRight(index + 1, x + offset, offset, callback);
+        }
+      });
+    } else if (index == 10) {
+      if  (Stage.dinoDirection == "left") {
+        Stage.dino.animate({x: x - offset}, 100, 'linear', function() {
+          Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/l1.png');
+          callback();
+        });
+      } else {
+        Stage.dino.animate({x: x + offset}, 100, 'linear', function() {
+          Stage.dino.attr('src', 'images/' + Stage.spriteDir + '/1.png');
+          callback();
+        });
+      }
+    }
+
+  },
+
+  animateLeft: function(index, x, offset, callback) {
+    if (x + offset > Stage.width - 97  || x + offset < 0) {
+      offset = 0;
+    }
+    if (index < 10) {
+      Stage.dino.animate({x: x - offset}, 100, 'linear', function() {
+        if  (Stage.dinoDirection == "right") {
+          Stage.dino.attr('src', 'images/'+ Stage.spriteDir +'/' + index + '.png');
+          PrivateMethods.animateRight(index + 1, x - offset, -offset, callback);
+        } else {
+          Stage.dino.attr('src', 'images/'+ Stage.spriteDir +'/l' + index + '.png');
+          PrivateMethods.animateLeft(index + 1, x + offset, offset, callback);
+        }
+      });
+    } else if (index == 10) {
+      if  (Stage.dinoDirection == "right") {
+        Stage.dino.animate({x: x - offset}, 100, 'linear', function() {
+          Stage.dino.attr('src', 'images/'+ Stage.spriteDir +'/1.png');
+          callback();
+        });
+      } else {
+        Stage.dino.animate({x: x + offset}, 100, 'linear', function() {
+          Stage.dino.attr('src', 'images/'+ Stage.spriteDir +'/l1.png');
+          callback();
+        });
+      }
+    }
+  },
+
+  shake: function(callback, command, commandList) {
+    var prevX = 1.0;
+    var axl = new Accelerometer();
+    var threshold = 0.5;
+    axl.watchAcceleration(
+      function (Accel)
+      {
+        if (true === Accel.is_updating){
+          return;
+        }
+        var diffX = Math.abs(Accel.x) - prevX;
+
+        if (diffX >= threshold)
+        {
+          Program.executeEvent(command);
+        }
+        prevX = Math.abs(Accel.x);
+      },
+      function(){},
+      {frequency : 100}
+    );
+    callback(command, commandList);
+  },
+
+  touch: function(callback, command, commandList) {
+    $("#print-area").unbind("touchstart");
+    $("#print-area").bind("touchstart", function() {
+      Program.executeEvent(command);
+    });
+    callback(command, commandList);
+  }
+}
---
+++ platforms/browser/www/javascripts/on_load.js
@@ -0,0 +1,230 @@
+var CommandLibrary = {
+  init: function() {
+    _.each(_.keys(Methods), function(method) {
+      if (method == "repeat") {
+        $("<li class='loop command'><span class='name'>repeat</span></li>").append(
+          $("<span class='times'> 5</span>")).appendTo("#methods");
+        } else {
+        var command = $("<li class='command "+method+"'><span class='name'>"
+          + method + "</span></li>");
+        if (Methods[method].args.length > 0) {
+          command.append('<select class="args">'+
+            _.foldl(Methods[method].args,function(memo, option){
+              return memo+"<option>"+option+"</option>" }, '')+"</select></form>");
+        }
+        $('#methods').append(command)
+      }
+    });
+  }
+}
+
+var Program = {
+  attachNestedSortables: function (e, ui) {
+    $('.command-list > li').each(function(i, e){
+      if($(this).attr('id') == undefined) {
+        var id = Date.now();
+        $(this).attr('id', "command-"+id);
+        if ($(this).hasClass("loop")||$(this).hasClass('when')) {
+          $("#command-area > .command-list").addClass("with-loop");
+          var nestedID = 'loop-'+id
+          $('.program').append("<div class='nestedCommands' id='loop-"+id+"'>"+
+            "<div class='loop-commands'><ul class='command-list'>"+
+            "</li></ul></div><div class='bottom'></div></div>");
+          Program.init()
+        }
+      }
+    })
+    $('.nestedCommands').each(function(){
+      var id = _.last($(this).attr('id').split('-'))
+      if ($('#command-'+id).length > 0) {
+        var pos = $('#command-'+id).offset()
+        $('#loop-'+id).css({top: pos.top+12+'px', left: pos.left+138+'px'});
+      } else {
+        $(this).remove();
+      }
+    })
+  },
+  init: function(){
+    $(".command-list").sortable({
+      placeholder: "ui-state-highlight",
+      stop: Program.attachNestedSortables,
+      receive: Program.attachNestedSortables,
+      connectWith: '.command-list'
+    });
+  },
+  executeMethods: function(command, rest) {
+    if (rest.length == 0) {
+      command.removeClass("active");
+      if ($('body.tutorial-page').length > 0) {
+        tutorial.runSpec();
+      }
+      return;
+    } else {
+      command.removeClass("active");
+      command = _.first(rest);
+      command.addClass("active");
+
+      var name = command.find('.name').html();
+      rest = _.rest(rest, 1);
+      if(command.find('.args').length == 0) {
+        Analytics.record("played_"+name);
+      } else {
+        Analytics.record("played_"+name+"_"+command.find('.args').val());
+      }
+      Methods[name].call(command.find('.args').val(), Program.executeMethods, command, rest);
+    }
+  },
+  executeEvent: function(command) {
+    var id = command.attr("id").split("-")[1];
+    Program.parse("loop-" + id);
+  },
+  parse: function(id) {
+    var flattenCommand = function(command) {
+      if ($(command).find(".name").html() == "repeat") {
+        var range = $(command).find(".times").html();
+        var loopBodyID = "#loop-"+_.last(command.id.split('-'));
+        var array = _.map(_.range(range), function() {
+          return _.map($(loopBodyID).find(".command"), function(commandInner) {
+            if ($(commandInner).find(".name").html() == "repeat") {
+              return flattenCommand(commandInner);
+            } else {
+              return $(commandInner);
+            }
+          });
+        });
+        return array;
+      } else {
+        return $(command);
+      }
+    };
+    var commands = _.flatten(_.map($("#" + id + " .command-list > .command"), function(command) {
+      return flattenCommand(command);
+    }));
+
+    var command = _.first(commands);
+    Program.executeMethods(command, commands);
+  },
+  expandLoop: function(event) {
+    var loopCommands = $(event.target).parents(".loop").find(".loop-commands").first();
+    var dialog = loopCommands.dialog({
+      title: "Drag commands here to add them to the loop",
+      zIndex: 99,
+      close: function() {
+        $(event.target).parents("li").find('.nestedCommands').html("");
+        loopCommands.clone(true).appendTo($(event.target).parents("li").find('.nestedCommands'));
+        $(event.target).parents("li").find('.nestedCommands').append('<div class="bottom"></div>');
+        $("#methods .command").draggable({ revert: true,
+          revertDuration: 0,
+          helper: 'clone',
+          connectToSortable: '.command-list'
+        });
+      }
+    });
+    $(dialog).find('ul').first().sortable({ placeholder: 'ui-state-highlight'});
+    $("#methods .command").draggable({
+      revert: true,
+      revertDuration: 0,
+      helper: 'clone',
+      connectToSortable: $(dialog).find('ul')
+    });
+  }
+};
+
+var Controls = {
+  play: function(e){
+    e.preventDefault();
+    if( $(".command-list > .command").length == 0 ) {
+      var dialog = $("#play-dialog").dialog({
+        title: $("#play-dialog").data('title'),
+        resizable: false,
+        modal: true,
+        width: 400,
+        buttons: {
+          "Ok": function() {
+            $(this).dialog('close');
+          }
+        }
+      });
+    }
+    else {
+      Stage.dino.animate({x: Stage.position.x, y: Stage.position.y}, 0, 'linear', function() {
+        Stage.dinoDirection = "right";
+        Stage.dino.data("scale", 0)
+        Stage.spriteDir = "sprites"
+        Stage.dino.attr({"src" : "images/sprites/1.png", height: 80, width: 70});
+        if ($('.program .nestedCommands').length > 1) {
+          Analytics.record("pressed_play_with_two_loops")
+        }
+        Program.parse("command-area");
+      });
+    }
+  }
+}
+
+$(function() {
+  Stage.init();
+  Analytics.init();
+  CommandLibrary.init();
+  Program.init();
+  $("#share").click(Controls.share);
+  $("#help").click(Controls.help);
+  $("#play").click(Controls.play);
+
+  $('head meta[name=viewport]').remove();
+  $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0" />');
+
+  $("#methods .command").bind("mousedown", function() {
+    $(".command-list").addClass("highlight");
+    if($('.command-list li').length == 0) {
+      $('.arrow').show();
+      var pos = $(this).offset()
+      $('.arrow').css({top: pos.top-30+'px', left: pos.left+50+'px'});
+      $('.arrow').animate({left:pos.left+150+'px', opacity: '1'},'slow','swing',function(){
+        $('.arrow').animate({opacity:'0'},1500,function(){
+          $('.arrow').hide();
+        });
+      });
+    }
+  });
+  $("#methods .command").bind("touchend", function() {
+    $(".command-list").removeClass("highlight");
+  });
+  $("#methods .command").bind("touchstart", function() {
+    $(".command-list").addClass("highlight");
+    if($('.command-list li').length == 0) {
+      $('.arrow').show();
+      var pos = $(this).offset()
+      $('.arrow').css({top: pos.top-30+'px', left: pos.left+50+'px'});
+      $('.arrow').animate({left:pos.left+150+'px', opacity: '1'},'slow','swing',function(){
+        $('.arrow').animate({opacity:'0'},1500,function(){
+          $('.arrow').hide();
+        });
+      });
+    }
+  });
+
+  $("#methods .command").draggable({
+    revert: true,
+    revertDuration: 0,
+    helper: 'clone',
+    connectToSortable: '.command-list'
+  });
+
+
+  $("#command-library").droppable({
+    accept: ".command-list .command, .loop-commands .command",
+    activeClass: 'trash-highlight',
+    drop: function(event, ui) {
+      ui.draggable.remove();
+      if ($(".command-list").find(".command-list").length == 0) {
+        $("#command-area > .command-list").removeClass("with-loop");
+      }
+    }
+  });
+});
+
+$('select').live('change', function(){
+  var selected = $(this).find('option:selected');
+  $(this).find('option').attr('selected', false);
+  selected.attr('selected', 'selected');
+});
