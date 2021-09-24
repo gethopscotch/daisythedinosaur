ec1bbab750385f195f122c0ba219d973bbc32fd1
Samantha John <samantha@gethopscotch.com>
2021-09-24 19:23:05 +0200

    Make a shell app to get it running on iOS 15

--- DaisyIOS/Daisy/ViewController.swift
+++ DaisyIOS/Daisy/ViewController.swift
@@ -10,17 +10,36 @@ import WebKit
 
 class ViewController: UIViewController {
 
-    let webview = WKWebView(frame: .zero)
+    let webView = WKWebView(frame: .zero)
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(webview)
-        webview.loadHTMLString("hello world", baseURL: nil)
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9122232199, green: 0.9865009189, blue: 0.9939107299, alpha: 1)
+        view.addSubview(webView)
+        webView.scrollView.isScrollEnabled = false
+        webView.isOpaque = false
+        webView.backgroundColor = #colorLiteral(red: 0.9122232199, green: 0.9865009189, blue: 0.9939107299, alpha: 1)
+        let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "www")!
+        webView.loadFileURL(url, allowingReadAccessTo: url)
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
-    
+
+    var html: String {
+        let filePath =  Bundle.main.path(forResource: "index", ofType: "html", inDirectory: "www") ?? ""
+        do {
+            return try NSString(contentsOfFile: filePath, encoding: String.Encoding.utf8.rawValue) as String
+        } catch {
+            return ""
+        }
+    }
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        webview.frame = view.frame
+        let webWidth = CGFloat(1024)
+        let webHeight = CGFloat(768)
+        webView.frame = view.frame
+        let scaleX = view.frame.size.width / webWidth
+        let scaleY = view.frame.size.height / webHeight
+        webView.transform = CGAffineTransform.init(scaleX: scaleX, y: scaleY)
     }
     
     override var prefersStatusBarHidden: Bool { true }
